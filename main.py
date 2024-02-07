218457799742a4e8088aab13f271d1972a17cbe4
jihun <altmxjdms@naver.com>
2024-02-07 15:52:25 +0900

    v1,1

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
+num=int(input())
+
+num_list=[]
+for i in range(num):
+    a=int(input())
+    num_list.append(a)
 
 
-print("hello")
 
-print("hello")
+
+if(sum(num_list)/len(num_list)>=(sum(num_list)//len(num_list))+0.5):
+    print(sum(num_list)//len(num_list)+1)
+else:
+    print(sum(num_list) // len(num_list) )
+
+num_list.sort()
+
+print(num_list[num//2])
+
+print(num_list[-1]-num_list[0])
+
